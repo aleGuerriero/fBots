31829be2c9a0cfd4e2c35b7276e22affe28cbc87
aleGuerriero <alessandro.guerriero@live.it>
2024-11-26 14:49:57 +0100

    Fixed and enhanced alpha-beta search

--- BattleTester.py
+++ BattleTester.py
@@ -1,5 +1,9 @@
-from bots.BattlePolicies import FirstPlayer
+from tqdm import tqdm
+
+from bots.BattlePolicies import AlphaBetaPolicy
 from bots.fCompetitor import fCompetitor
+from bots.Thunder_BattlePolicies import ThunderPlayer
+from bots.hayo5 import hayo5_BattlePolicy
 
 from vgc.competition.BattleMatch import BattleMatch
 from vgc.competition.Competitor import CompetitorManager
@@ -9,18 +13,29 @@ from vgc.util.generator.PkmTeamGenerators import RandomTeamFromRoster
 from vgc.behaviour.BattlePolicies import TerminalPlayer, Minimax
 
 def main():
-  roster = RandomPkmRosterGenerator().gen_roster()
-  tg = RandomTeamFromRoster(roster)
+  n_matches: int = 100
+  debug: bool = False
+
   c0 = fCompetitor('Player1')
-  c0._battle_policy = Minimax()
   cm0 = CompetitorManager(c0)
-  cm0.team = tg.get_team()
   c1 = fCompetitor('Player2')
-  c1._battle_policy = TerminalPlayer()
+  c1._battle_policy = ThunderPlayer()
   cm1 = CompetitorManager(c1)
-  cm1.team = tg.get_team()
-  match = BattleMatch(cm0, cm1, debug=True)
-  match.run()
+  roster = RandomPkmRosterGenerator().gen_roster()
+
+  wins: int = 0
+  for _ in (pbar := tqdm(range(n_matches), leave=True)):
+    tg = RandomTeamFromRoster(roster)
+    cm0.team = tg.get_team()
+    cm1.team = tg.get_team()
+    match = BattleMatch(cm0, cm1, debug=debug)
+    match.run()
+    # print(match.winner())
+    # print(f'{match.cms[match.winner()].competitor.name} won')
+    wins += match.winner() == 0
+    pbar.set_description(f'Current wins: {wins}')
+
+  print(f'{c0.name} won: {wins}/{n_matches} matches')
 
 if __name__=='__main__':
   main()
--- bots/BattlePolicies.py
+++ bots/BattlePolicies.py
@@ -1,40 +1,181 @@
 from typing import Any, List, Union
+from copy import deepcopy
 
-from collections import namedtuple
+import numpy as np
 
 from vgc.behaviour import BattlePolicy
-from vgc.datatypes.Objects import GameState
+from vgc.datatypes.Types import PkmStatus
+from vgc.datatypes.Objects import GameState, PkmTeam, PkmType, Pkm
+from vgc.datatypes.Constants import DEFAULT_N_ACTIONS, TYPE_CHART_MULTIPLIER
 
-class Node(namedtuple('NodeBase',['a', 'g', 'parent', 'depth', 'eval'])):
-  __slots__ = () # using __slots__ to keep it lightweight
+class Node():
 
-  def path(self) -> List[int]:
-    '''
-    Reconstruct the path from the start state to this node
-    '''
-    node, path = self, []
-    while node.parent is not None:
-      path.append(node.a)
-      node = node.parent
-    return path[::-1]
+  def __init__(self):
+    self.action: int = None
+    self.gameState: GameState = None
+    self.parent: Node = None
+    self.depth: int = 0
+    self.value: float = 0.
+
+  def __str__(self):
+    return f'Node(action: {self.action}, depth: {self.depth}, value: {self.value}, parent: {str(self.parent)})'
   
-  def __repr__(self):
-    return f'Node(action={self.a}, value={self.eval})'
+def match_up_eval(my_pkm_type: PkmType,
+      opp_pkm_type: PkmType,
+      my_moves_type: List[PkmType],
+      opp_moves_type: List[PkmType]
+  ) -> float:
+  # determine defensive match up
+  defensive_match_up = 0.
+  for mtype in opp_moves_type:
+    if mtype == opp_pkm_type:
+      defensive_match_up = max(TYPE_CHART_MULTIPLIER[mtype][my_pkm_type]*1.5, defensive_match_up)
+    else:
+      defensive_match_up = max(TYPE_CHART_MULTIPLIER[mtype][my_pkm_type], defensive_match_up)
+  #print(f'DEFENSIVE MATCH UP: {defensive_match_up}')
+
+  offensive_match_up = 0.
+  for mtype in my_moves_type:
+    if mtype == opp_pkm_type:
+      offensive_match_up = max(TYPE_CHART_MULTIPLIER[mtype][opp_pkm_type]*1.5, offensive_match_up)
+    else:
+      offensive_match_up = max(TYPE_CHART_MULTIPLIER[mtype][my_pkm_type], offensive_match_up)
+  #print(f'OFFENSIVE MATCH UP: {offensive_match_up}')
+    
+  return offensive_match_up - defensive_match_up
+
+def stage_eval(team: PkmTeam) -> int:
+  stage: int = 0
+  for s in team.stage:
+    stage += s
+  return stage
+
+def status_eval(pkm: Pkm) -> float:
+  if pkm.status == (PkmStatus.CONFUSED or PkmStatus.PARALYZED or PkmStatus.SLEEP or PkmStatus.FROZEN):
+    return -1
+  elif pkm.status == (PkmStatus.BURNED or PkmStatus.POISONED):
+    return -0.5
+  else:
+    return 0
   
 def game_state_eval(g: GameState):
-  my_active = g.teams[0].active
-  opp_active = g.teams[0].active
-  return my_active.hp/my_active.max_hp - opp_active.hp/opp_active.max_hp
+  my_team = g.teams[0]
+  opp_team  = g.teams[1]
+  my_active: Pkm = my_team.active
+  opp_active: Pkm = opp_team.active
+  match_up: float = match_up_eval(my_active.type, opp_active.type,
+      list(map(lambda m: m.type, my_active.moves)),
+      list(map(lambda m: m.type, [move for move in opp_active.moves if move.name != None])))
+  #print(f'MATCH UP: {match_up}')
+  my_stage = stage_eval(my_team)
+  opp_stage = stage_eval(opp_team)
+  my_status = status_eval(my_active)
+  opp_status = status_eval(opp_active)
+  return (match_up 
+          + my_active.hp/my_active.max_hp
+          - opp_active.hp/opp_active.max_hp
+          + 0.2*my_stage
+          - 0.2*opp_stage
+          + my_status
+          - opp_status)
 
-class FirstPlayer(BattlePolicy):
+def n_fainted(team: PkmTeam) -> int:
+  fainted = 0
+  fainted += team.active.hp == 0
+  if len(team.party) > 0:
+    fainted += team.party[0].hp == 0
+  if len(team.party) > 1:
+    fainted += team.party[1].hp == 0
+  return fainted
 
-  def __init__(self, max_depth: int = 3):
+class AlphaBetaPolicy(BattlePolicy):
+
+  def __init__(self, max_depth: int = 6):
     self.max_depth = max_depth
 
   def get_action(self, g: Union[List[float], GameState]) -> int:
     root: Node = Node()
-    root.g = g
-    node_queue: List[Node] = [root]
+    root.gameState = g
 
-    while len(node_queue) > 0 and node_queue[0].depth < self.max_depth:
-      curr_parent = node_queue.pop(0)
+    #print('---------------------------------')
+    # print('OPPONENT MOVES')
+    # for i in range(DEFAULT_N_ACTIONS-2):
+    #   print(f'{str(g.teams[1].active.moves[i])}')
+    # print('OPPONENT PKM')
+    # print(g.teams[1])
+    # print('---------------------------------')
+
+    action = self._alphaBeta_search(root)
+    return action
+
+  def _alphaBeta_search(
+      self,
+      root: Node,
+      alpha: float = -np.inf,
+      beta: float = np.inf
+  ) -> int:
+    #print("ALPHA BETA SEARCH")
+    value, move = self._max_value(root, alpha, beta)
+    #print('---------------------------------')
+    #print(f'AlphaBetaPolicy chose action: {root.gameState.teams[0].active.moves[move]}, with value: {value}')
+    #print('---------------------------------')
+    return move
+
+  def _max_value(
+      self,
+      node: Node,
+      alpha: float,
+      beta: float
+  ) -> tuple[float, Union[int, None]]:
+    state: GameState = deepcopy(node.gameState)
+    node.value = game_state_eval(state)
+    # print('---------------------------------')
+    # print(f'CURRENT NODE: {str(node)}')
+    # print('---------------------------------')
+    # print(f'MY HP: {state.teams[1].active.hp}')
+    # print(f'OPPONENT HP: {state.teams[1].active.hp}')
+    if state.teams[1].active.hp == 0 or state.teams[0].active.hp == 0 or node.depth >= self.max_depth:
+      return game_state_eval(state), None
+    value = -np.inf
+    for i in range(DEFAULT_N_ACTIONS):
+      next_node: Node = Node()
+      next_node.parent = node
+      next_node.depth = node.depth + 1
+      next_node.action = i
+      next_node.gameState = state
+      next_node.value, _ = self._min_value(next_node, alpha, beta)
+      # print('---------------------------------')
+      # print(f'NEXT NODE: {str(next_node)}')
+      # print('---------------------------------')
+      if next_node.value > value:
+        value, move = next_node.value, next_node.action
+        alpha = max(value, alpha)
+      if value >= beta:
+        return value, move
+    return value, move
+        
+  def _min_value(
+      self,
+      node: Node,
+      alpha: float,
+      beta: float
+  ) -> tuple[float, Union[int, None]]:
+    state: GameState = deepcopy(node.gameState)
+    if state.teams[1].active.hp == 0 or state.teams[0].active.hp == 0 or node.depth >= self.max_depth:
+      return game_state_eval(state), None
+    value = np.inf
+    for i in range(DEFAULT_N_ACTIONS):
+      next_state, _, _, _, _ = state.step([node.action, i])
+      next_node: Node = Node()
+      next_node.parent = node
+      next_node.depth = node.depth + 1
+      next_node.action = i
+      next_node.gameState = next_state[0]
+      next_node.value, _ = self._max_value(next_node, alpha, beta)
+      if next_node.value < value:
+        value, move = next_node.value, next_node.action
+        beta = min(value, beta)
+      if value <= alpha:
+        return value, move
+    return value, move
+
