8d696264210965386ff671cfc60cbd66a4176a43
Elia-bocc <80979014+Elia-bocc@users.noreply.github.com>
2024-12-13 16:28:28 +0100

    Update GreedyPolicy.py

--- bots/GreedyPolicy.py
+++ bots/GreedyPolicy.py
@@ -11,17 +11,6 @@ from vgc.datatypes.Objects import GameState, PkmTeam, PkmType, Pkm, PkmMove, Pkm
 from vgc.datatypes.Constants import DEFAULT_N_ACTIONS, TYPE_CHART_MULTIPLIER
 from vgc.competition.StandardPkmMoves import STANDARD_MOVE_ROSTER
 
-class Node():
-
-  def __init__(self):
-    self.action: int = None
-    self.gameState: GameState = None
-    self.parent: Node = None
-    self.depth: int = 0
-    self.value: float = 0.
-
-  def __str__(self):
-    return f'Node(action: {self.action}, depth: {self.depth}, value: {self.value}, parent: {str(self.parent)})'
   
 def match_up_eval(my_pkm_type: PkmType,
       opp_pkm_type: PkmType,
@@ -47,79 +36,6 @@ def match_up_eval(my_pkm_type: PkmType,
     
   return offensive_match_up - defensive_match_up
 
-def better_estimate_move(pkm: Pkm) -> None:
-  # controlla se è già presente una mossa del tipo del pokemon
-  type_m = sum([move.type==pkm.type for move in pkm.moves if move.name is not None])
-  for move_i in range(DEFAULT_N_ACTIONS-2):
-    if pkm.moves[move_i].name is None:
-      # se non è presente una mossa del tipo del pkm allora ne aggiungo una random
-      # forse potrebbe convenire aggiungere la più potente pensando al caso pessimo ed alla propria incolumità 
-      # tanto gli algoritmi avversari sono greedy sul danno
-      if type_m==0:
-        type_moves = [move for move in STANDARD_MOVE_ROSTER if move.type==pkm.type]
-        pkm.moves[move_i] = random.choice(type_moves)
-        type_m = 1
-      else:
-        # faccio in modo che sia diversa dalle mosse che ho già
-        move = random.choice(STANDARD_MOVE_ROSTER)
-        while(move in pkm.moves):
-          move = random.choice(STANDARD_MOVE_ROSTER)
-        pkm.moves[move_i] = move
-
-def known_opp_moves(pkm: Pkm) -> int:
-  known = 0
-  for move_i in range(DEFAULT_N_ACTIONS-2):
-    if pkm.moves[move_i].name is not None:
-      known += 1
-  return known
-
-def estimate_move(pkm: Pkm) -> None:
-  for move_i in range(DEFAULT_N_ACTIONS-2):
-    if pkm.moves[move_i].name is None:
-      if move_i == 0:
-        type_moves = [move for move in STANDARD_MOVE_ROSTER if move.type==pkm.type]
-        pkm.moves[move_i] = random.choice(type_moves)
-      else:
-        pkm.moves[move_i] = random.choice(STANDARD_MOVE_ROSTER)
-
-def stage_eval(team: PkmTeam) -> int:
-  stage: int = 0
-  for s in team.stage:
-    stage += s
-  return stage
-
-def status_eval(pkm: Pkm) -> float:
-  if pkm.status == (PkmStatus.CONFUSED or PkmStatus.PARALYZED or PkmStatus.SLEEP or PkmStatus.FROZEN):
-    return -1
-  elif pkm.status == (PkmStatus.BURNED or PkmStatus.POISONED):
-    return -0.5
-  else:
-    return 0
-
-def better_game_state_eval(g: GameState, depth: int):
-  my_team = g.teams[0]
-  opp_team  = g.teams[1]
-  my_active: Pkm = my_team.active
-  opp_active: Pkm = opp_team.active
-  match_up: float = match_up_eval(my_active.type, opp_active.type,
-      list(map(lambda m: m.type, my_active.moves)),
-      list(map(lambda m: m.type, [move for move in opp_active.moves if move.name != None])))
-  #print(f'MATCH UP: {match_up}')
-  my_stage = stage_eval(my_team)
-  opp_stage = stage_eval(opp_team)
-  my_status = status_eval(my_active)
-  opp_status = status_eval(opp_active)
-  return (match_up 
-          + my_active.hp/my_active.max_hp*3
-          - opp_active.hp/opp_active.max_hp*3
-          + 0.2*my_stage
-          - 0.2*opp_stage
-          + my_status
-          - opp_status
-          - 0.3*math.ceil(depth/2)
-          + (my_team.party[0].hp/my_team.party[0].max_hp+my_team.party[1].hp/my_team.party[1].max_hp)*2)
-# AGGIUNGERE LA VITA DELLA SQUADRA tipo + (party[0].hp/party[0].max_hp+party[1].hp/party[1].max_hp)
-# ma noi possiamo vedere la vita del party avversario?????
 
 def n_fainted(team: PkmTeam) -> int:
   fainted = 0
@@ -205,25 +121,8 @@ def calculateDamages(attack1:int, defense2:int, pkm1:Pkm, pkm2:Pkm, weather:Weat
 
 class GreedyPolicy(BattlePolicy):
 
-  def __init__(self, max_depth: int = 6, seed: int = 69):
-    self.max_depth = max_depth
-    random.seed(seed)
-    #momentanea
-    #self.policy = ThunderPlayer()
-    #self.policy2 = TypeSelector()
-
-  def get_action(self, g: Union[List[float], GameState]) -> int:
-    root: Node = Node()
-    root.gameState = g
-    
-    #print('---------------------------------')
-    # print('OPPONENT MOVES')
-    # for i in range(DEFAULT_N_ACTIONS-2):
-    #   print(f'{str(g.teams[1].active.moves[i])}')
-    # print('OPPONENT PKM')
-    # print(g.teams[1])
-    # print('---------------------------------')
-    return self._simple_search(root.gameState)
+  def get_action(self, g: GameState]) -> int:
+    return self._simple_search(g)
 
   def _simple_search(self, g: GameState) -> int:
 
